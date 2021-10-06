8cdaa315e551f41543beab990feb2ee47761c8db
tammehh <85746894+tammehh@users.noreply.github.com>
2021-10-06 15:10:21 +0800

    Update osheGui.py

--- osheGui.py
+++ osheGui.py
@@ -44,7 +44,129 @@ class StartPage(tk.Frame):
         tk.Button(self, text='Admin Login' , width=20,bg="orange",fg='white',command= lambda: master.switch_frame(AdminLogin)).pack(padx=10,pady=10)
         tk.Button(self, text='Customer Register' , width=20,bg="blue",fg='white',command= lambda: master.switch_frame(CustomerRegistration)).pack(padx=10,pady=10)
         tk.Button(self, text='Admin Register' , width=20,bg= "black",fg='white',command= lambda: master.switch_frame(AdminRegistration)).pack(padx=10,pady=10)
+       
+class CustomerRegistration(tk.Frame):
+    def validRegistration(self):
+        self.output_label = tk.Label(self)
+        self.output_label.pack()
+        self.output_label.config(text= name.get() + "'s Registration as Customer successful")
+        val = (username.get(), name.get(), add.get(), gender.get(), email.get(), number.get(), password.get())
+        sql = "INSERT INTO Customers (CustomerID, CustomerName, Address, Gender, EmailAddress, PhoneNumber, Password) VALUES " + str(val) + ";"
+        c.execute(sql)
 
+    def __init__(self, master):
+        global username
+        global name
+        global add
+        global gender
+        global email
+        global number
+        global password
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="Customer Registration", width = 20, font=("bold",20)).pack()
+
+        #accept input string text from user
+        usernameLabel = tk.Label(self,text = "Username: ", font = ("bold", 10))
+        usernameLabel.pack()
+        username = tk.StringVar()
+        usernameEntry = tk.Entry(self,textvariable=username, font = ("bold", 10))
+        usernameEntry.pack(pady=10)
+        
+        nameLabel = tk.Label(self,text = "Name: ", font = ("bold", 10))
+        nameLabel.pack()
+        name = tk.StringVar()
+        nameEntry = tk.Entry(self,textvariable=name, font = ("bold", 10))
+        nameEntry.pack(pady=10)
+
+        addLabel = tk.Label(self,text = "Address: ", font = ("bold", 10))
+        addLabel.pack()
+        add = tk.StringVar()
+        addEntry = tk.Entry(self,textvariable=add, font = ("bold", 10))
+        addEntry.pack(pady=10)
+
+        genderLabel = tk.Label(self,text = "Gender (M/F): ", font = ("bold", 10))
+        genderLabel.pack()
+        gender = tk.StringVar()
+        genderEntry = tk.Entry(self,textvariable=gender, font = ("bold", 10))
+        genderEntry.pack(pady=10)
+
+        emailLabel = tk.Label(self,text = "Email Address: ", font = ("bold", 10))
+        emailLabel.pack()
+        email = tk.StringVar()
+        emailEntry = tk.Entry(self,textvariable=email, font = ("bold", 10))
+        emailEntry.pack(pady=10)
+        
+        numberLabel = tk.Label(self,text = "Phone Number: ", font = ("bold", 10))
+        numberLabel.pack()
+        number = tk.StringVar()
+        numberEntry = tk.Entry(self,textvariable=number, font = ("bold", 10))
+        numberEntry.pack(pady=10)
+
+        passwordLabel = tk.Label(self,text = "Password: ", font = ("bold", 10))
+        passwordLabel.pack()
+        password = tk.StringVar()
+        passwordEntry = tk.Entry(self,textvariable=password, font = ("bold", 10))
+        passwordEntry.pack(pady=10)
+
+        tk.Button(self, text='REGISTER' , width=20,bg="black",fg='white',command= self.validRegistration).pack(pady = 10)
+        tk.Button(self, text='BACK' , width=20,bg="black",fg='white',command= lambda: master.switch_frame(StartPage)).pack()
+
+class AdminRegistration(tk.Frame):
+    def validRegistration(self):
+        self.output_label = tk.Label(self)
+        self.output_label.pack()
+        self.output_label.config(text= name.get() + "'s Registration as Administrator successful")
+        #start changes#
+        val = (username.get(), name.get(), gender.get(), number.get(), password.get())
+        sql = "INSERT INTO Administrator (AdministratorID, AdminName, Gender, PhoneNumber, Password) VALUES" + str(val) + ";" 
+        c.execute(sql)
+        #end changes#
+
+    def __init__(self, master):
+        global username
+        global name
+        global gender
+        global number
+        global password
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="Administrator Registration", width = 20, font=("bold",20)).pack()
+
+        #accept input string text from user
+        usernameLabel = tk.Label(self,text = "Username: ", font = ("bold", 10))
+        usernameLabel.pack()
+        username = tk.StringVar()
+        usernameEntry = tk.Entry(self,textvariable=username, font = ("bold", 10))
+        usernameEntry.pack(pady=10)
+        
+        nameLabel = tk.Label(self,text = "Name: ", font = ("bold", 10))
+        nameLabel.pack()
+        name = tk.StringVar()
+        nameEntry = tk.Entry(self,textvariable=name, font = ("bold", 10))
+        nameEntry.pack(pady=10)
+
+        genderLabel = tk.Label(self,text = "Gender (M/F): ", font = ("bold", 10))
+        genderLabel.pack()
+        gender = tk.StringVar()
+        genderEntry = tk.Entry(self,textvariable=gender, font = ("bold", 10))
+        genderEntry.pack(pady=10)
+        
+        numberLabel = tk.Label(self,text = "Phone Number: ", font = ("bold", 10))
+        numberLabel.pack()
+        number = tk.StringVar()
+        numberEntry = tk.Entry(self,textvariable=number, font = ("bold", 10))
+        numberEntry.pack(pady=10)
+
+        passwordLabel = tk.Label(self,text = "Password: ", font = ("bold", 10))
+        passwordLabel.pack()
+        password = tk.StringVar()
+        passwordEntry = tk.Entry(self,textvariable=password, font = ("bold", 10))
+        passwordEntry.pack(pady=10)
+
+        
+        tk.Button(self, text='REGISTER' , width=20,bg="black",fg='white',command= self.validRegistration).pack(pady = 10)
+        tk.Button(self, text='BACK' , width=20,bg="black",fg='white',command= lambda: master.switch_frame(StartPage)).pack()
+     
+        
 class CustLogin(tk.Frame):
     def validLogin(self):
         problem = ""
@@ -74,6 +196,7 @@ class CustLogin(tk.Frame):
                     messagebox.showerror(title="ERROR",message=problem)
                 else:
                     global customerName
+                    global custId
                     custId = row[0]
                     customerName = row[1]
                     messagebox.showinfo(message="Successful login, Welcome " + customerName)
@@ -125,17 +248,18 @@ class AdminLogin(tk.Frame):
         if(problem != ""):
             return messagebox.showerror(title="ERROR",message=problem)
 
-        check_query = "SELECT * FROM Administrator WHERE AdminID=? AND Password=?"
+        check_query = "SELECT * FROM Administrator WHERE AdministratorID=? AND Password=?"
         if check == 2:
             try:
                 c.execute(check_query,(username.get(),password.get()))
-              
+
                 row = c.fetchone()
                 if row == None:
                     problem +="\n"
                     problem += "Invalid login information"
                     messagebox.showerror(title="ERROR",message=problem)
                 else:
+                    global adminID
                     global adminName
                     adminID = row[0]
                     adminName = row[1]
@@ -143,12 +267,7 @@ class AdminLogin(tk.Frame):
                     self.master.switch_frame(adminHome)
             except Exception as e:
                 messagebox.showerror(title="ERROR",message="SystemERROR")
-            
-
-    def validLogin(self):
-        self.output_label = tk.Label(self)
-        self.output_label.pack()
-        self.output_label.config(text= username.get() + "Login successful")
+        
 
     def __init__(self, master):
         global username
@@ -192,6 +311,8 @@ class custHome(tk.Frame):
         tk.Button(self, text='LOGOUT' , width=20,bg="blue",fg='white',command= lambda: master.switch_frame(StartPage)).pack(pady=5)
     def inventory(self): 
         pass
+    
+    
 class adminHome(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
@@ -207,120 +328,8 @@ class adminHome(tk.Frame):
         tk.Button(self, text='Payments' , width=20,bg="grey",fg='white',command= lambda: master.switch_frame(CustLogin)).pack(pady=5)
         tk.Button(self, text='Past purchase' , width=20,bg="orange",fg='white',command= lambda: master.switch_frame(AdminLogin)).pack(pady=5)
         tk.Button(self, text='LOGOUT' , width=20,bg="blue",fg='white',command= lambda: master.switch_frame(StartPage)).pack(pady=5)       
-
         
-class CustomerRegistration(tk.Frame):
-    def validRegistration(self):
-        self.output_label = tk.Label(self)
-        self.output_label.pack()
-        self.output_label.config(text= name.get() + "'s Registration as Customer successful")
-        val = ("1234567890", name.get(), add.get(), gender.get(), email.get(), number.get(), password.get())
-        sql = "INSERT INTO Customer (CustomerID, CustomerName, Gender, EmailAddress, PhoneNumber, Password) VALUES " + str(val) + ";"
-        c.execute(sql)
 
-    def __init__(self, master):
-        global name
-        global add
-        global gender
-        global email
-        global number
-        global password
-        tk.Frame.__init__(self, master)
-        tk.Label(self, text="Administrator Registration", width = 20, font=("bold",20)).pack()
-
-        #accept input string text from user
-        nameLabel = tk.Label(self,text = "Name: ", font = ("bold", 10))
-        nameLabel.pack()
-        name = tk.StringVar()
-        nameEntry = tk.Entry(self,textvariable=name, font = ("bold", 10))
-        nameEntry.pack(pady=10)
-
-        addLabel = tk.Label(self,text = "Address: ", font = ("bold", 10))
-        addLabel.pack()
-        add = tk.StringVar()
-        addEntry = tk.Entry(self,textvariable=add, font = ("bold", 10))
-        addEntry.pack(pady=10)
-
-        genderLabel = tk.Label(self,text = "Gender (M/F): ", font = ("bold", 10))
-        genderLabel.pack()
-        gender = tk.StringVar()
-        genderEntry = tk.Entry(self,textvariable=gender, font = ("bold", 10))
-        genderEntry.pack(pady=10)
-
-        emailLabel = tk.Label(self,text = "Email Address: ", font = ("bold", 10))
-        emailLabel.pack()
-        email = tk.StringVar()
-        emailEntry = tk.Entry(self,textvariable=email, font = ("bold", 10))
-        emailEntry.pack(pady=10)
-        
-        numberLabel = tk.Label(self,text = "Phone Number: ", font = ("bold", 10))
-        numberLabel.pack()
-        number = tk.StringVar()
-        numberEntry = tk.Entry(self,textvariable=number, font = ("bold", 10))
-        numberEntry.pack(pady=10)
-
-        passwordLabel = tk.Label(self,text = "Password: ", font = ("bold", 10))
-        passwordLabel.pack()
-        password = tk.StringVar()
-        passwordEntry = tk.Entry(self,textvariable=password, font = ("bold", 10))
-        passwordEntry.pack(pady=10)
-
-        #change this code to iterate and find avail codes
-        ID = "1234567890"
-        #
-        tk.Button(self, text='REGISTER' , width=20,bg="black",fg='white',command= self.validRegistration).pack(pady = 10)
-        tk.Button(self, text='BACK' , width=20,bg="black",fg='white',command= lambda: master.switch_frame(StartPage)).pack()
-
-class AdminRegistration(tk.Frame):
-    def validRegistration(self):
-        self.output_label = tk.Label(self)
-        self.output_label.pack()
-        self.output_label.config(text= name.get() + "'s Registration as Administrator successful")
-        #start changes#
-        val = ("1234567890", name.get(), gender.get(), number.get(), password.get())
-        sql = "INSERT INTO Customers(CustomerID, CustomerName, Address, Gender, EmailAddress, PhoneNumber, Password) VALUES" + str(val) + ";" 
-        c.execute(sql)
-        #end changes#
-
-    def __init__(self, master):
-        global name
-        global gender
-        global number
-        global password
-        tk.Frame.__init__(self, master)
-        tk.Label(self, text="Administrator Registration", width = 20, font=("bold",20)).pack()
-
-        #accept input string text from user
-        nameLabel = tk.Label(self,text = "Name: ", font = ("bold", 10))
-        nameLabel.pack()
-        name = tk.StringVar()
-        nameEntry = tk.Entry(self,textvariable=name, font = ("bold", 10))
-        nameEntry.pack(pady=10)
-
-        genderLabel = tk.Label(self,text = "Gender (M/F): ", font = ("bold", 10))
-        genderLabel.pack()
-        gender = tk.StringVar()
-        genderEntry = tk.Entry(self,textvariable=gender, font = ("bold", 10))
-        genderEntry.pack(pady=10)
-        
-        numberLabel = tk.Label(self,text = "Phone Number: ", font = ("bold", 10))
-        numberLabel.pack()
-        number = tk.StringVar()
-        numberEntry = tk.Entry(self,textvariable=number, font = ("bold", 10))
-        numberEntry.pack(pady=10)
-
-        passwordLabel = tk.Label(self,text = "Password: ", font = ("bold", 10))
-        passwordLabel.pack()
-        password = tk.StringVar()
-        passwordEntry = tk.Entry(self,textvariable=password, font = ("bold", 10))
-        passwordEntry.pack(pady=10)
-
-        #change this code to iterate and find avail codes
-        ID = "1234567890"
-        #
-        tk.Button(self, text='REGISTER' , width=20,bg="black",fg='white',command= self.validRegistration).pack(pady = 10)
-        tk.Button(self, text='BACK' , width=20,bg="black",fg='white',command= lambda: master.switch_frame(StartPage)).pack()
-     
 
 if __name__ == "__main__":
     conn = sqlite3.connect('OSHE') 
