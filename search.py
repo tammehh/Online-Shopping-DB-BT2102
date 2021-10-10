9e0ef5021d17559c2a9e500f4c43063bd3cca766
tammehh <85746894+tammehh@users.noreply.github.com>
2021-10-11 02:07:32 +0800

    Create search.py
    purchase function not included yet

---
+++ search.py
@@ -0,0 +1,246 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+import sqlite3
+import json
+import numpy as np
+import pandas as pd
+from pymongo import MongoClient
+from sqlalchemy import create_engine
+#Inventory table
+from custTable import *
+from request import *
+from adminFunc import *
+
+
+def simSearchTable(price, category, model, color, factory, powersupply, productionyear):
+    #initializing screen
+    root = Tk()
+    root.title('Your Search Results')
+    root.geometry("1000x500")
+
+    conn = sqlite3.connect('OSHE')
+    mongodb = MongoClient('localhost', 27017)
+    db = mongodb.OSH 
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure("Treeview",
+    background="#D3D3D3",
+    foreground="black",
+    rowheight=25,
+    fieldbackground="#D3D3D3")
+    style.map('Treeview',
+    background=[('selected', "#347083")])
+
+    #creating tree view
+    tree_frame = Frame(root)
+    tree_frame.pack(pady=10)
+
+    # Create a Treeview Scrollbar
+    tree_scroll = Scrollbar(tree_frame)
+    tree_scroll.pack(side=RIGHT, fill=Y)
+
+    # Create The Treeview
+    my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")
+    my_tree.pack()
+
+    # Configure the Scrollbar
+    tree_scroll.config(command=my_tree.yview)
+
+    # Define Our Columns
+    my_tree['columns'] = ("ItemID", "Category", "Model")
+
+    # Format Our Columns
+    my_tree.column("#0", width=0, stretch=NO)
+    my_tree.column("ItemID", anchor=W, width=50)
+    my_tree.column("Category", anchor=CENTER, width=90)
+    my_tree.column("Model", anchor=CENTER, width=90)
+
+
+    # Create Headings
+    my_tree.heading("#0", text="", anchor=W)
+    my_tree.heading("ItemID",text="ItemID", anchor=W)
+    my_tree.heading("Category",text="Category", anchor=CENTER)
+    my_tree.heading("Model", text="Model", anchor=CENTER)
+
+    # Create Striped Row Tags
+    my_tree.tag_configure('oddrow', background="white")
+    my_tree.tag_configure('evenrow', background="lightblue")
+    
+    global count
+    count = 0
+
+    allpdt = []
+    returnedresults = []
+    if category == "None":
+        category = db.Items.distinct("0.Category")
+    else:
+        category = [category]
+    if model == "None":
+        model = db.Items.distinct("0.Model")
+    else:
+        model = [model]
+    for x in category:
+        for y in model:
+            item = (db.Items.find_one({"0.Category": str(x), "0.Model": str(y), "0.PurchaseStatus": "Unsold"}))
+            print(item)
+            if item:
+                if count % 2 == 0:
+                    my_tree.insert(parent='', index='end',iid=count,text='',values=(item['0']['ItemID'],item['0']['Category'],item['0']['Model']), tags=('evenrow',))
+                else:
+                    my_tree.insert(parent='', index='end',iid=count,text='',values=(item['0']['ItemID'],item['0']['Category'],item['0']['Model']), tags=('oddrow',))
+                count += 1
+
+    
+def advSearchTable(price, category, model, color, factory, powersupply, productionyear):
+    #initializing screen
+    root = Tk()
+    root.title('Your Search Results')
+    root.geometry("1000x500")
+
+    conn = sqlite3.connect('OSHE')
+    mongodb = MongoClient('localhost', 27017)
+    db = mongodb.OSH 
+
+    global count
+    count = 0
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure("Treeview",
+    background="#D3D3D3",
+    foreground="black",
+    rowheight=25,
+    fieldbackground="#D3D3D3")
+    style.map('Treeview',
+    background=[('selected', "#347083")])
+
+    #creating tree view
+    tree_frame = Frame(root)
+    tree_frame.pack(pady=10)
+
+    # Create a Treeview Scrollbar
+    tree_scroll = Scrollbar(tree_frame)
+    tree_scroll.pack(side=RIGHT, fill=Y)
+
+    # Create The Treeview
+    my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")
+    my_tree.pack()
+
+    # Configure the Scrollbar
+    tree_scroll.config(command=my_tree.yview)
+
+    # Define Our Columns
+    my_tree['columns'] = ("ItemID", "Category", "Model", "Price", "Colour", "Factory",
+                        "PowerSupply","Production Year")
+
+    # Format Our Columns
+    my_tree.column("#0", width=0, stretch=NO)
+    my_tree.column("ItemID", anchor=W, width=50)
+    my_tree.column("Category", anchor=CENTER, width=90)
+    my_tree.column("Model", anchor=CENTER, width=90)
+    my_tree.column("Price", anchor=CENTER, width=70)
+    my_tree.column("Colour", anchor=CENTER, width=90)
+    my_tree.column("Factory", anchor=CENTER, width=100)
+    my_tree.column("PowerSupply", anchor=CENTER, width=90)
+    my_tree.column("Production Year", anchor=CENTER, width=100)
+
+
+    # Create Headings
+    my_tree.heading("#0", text="", anchor=W)
+    my_tree.heading("ItemID",text="ItemID", anchor=W)
+    my_tree.heading("Category",text="Category", anchor=CENTER)
+    my_tree.heading("Model", text="Model", anchor=CENTER)
+    my_tree.heading("Price", text="Price",anchor=CENTER)
+    my_tree.heading("Colour", text ="Colour", anchor=CENTER)
+    my_tree.heading("Factory", text="Factory", anchor=CENTER)
+    my_tree.heading("PowerSupply",text="PowerSupply", anchor=CENTER)
+    my_tree.heading("Production Year", text="Production Year", anchor=CENTER)
+
+    # Create Striped Row Tags
+    my_tree.tag_configure('oddrow', background="white")
+    my_tree.tag_configure('evenrow', background="lightblue")
+
+    # Create Striped Row Tags
+    my_tree.tag_configure('oddrow', background="white")
+    my_tree.tag_configure('evenrow', background="lightblue")
+    
+    data_frame = LabelFrame(root, text="Purchase Information")
+    data_frame.pack(fill="x", expand="yes", padx=10)
+
+    itemId_label = Label(data_frame, text="ItemID")
+    itemId_label.grid(row=0, column=2, padx=10, pady=10)
+    itemId_entry = Entry(data_frame)
+    itemId_entry.grid(row=0, column=3, padx=10, pady=10)
+
+    model_label = Label(data_frame, text="Model")
+    model_label.grid(row=0, column=4, padx=10, pady=10)
+    model_entry = Entry(data_frame)
+    model_entry.grid(row=0, column=5, padx=10, pady=10)
+
+    
+    if category == "None":
+        category = db.Items.distinct("0.Category")
+    else:
+        category = [category]
+    if model == "None":
+        model = db.Items.distinct("0.Model")
+    else:
+        model = [model]
+    if color == "None":
+        color = db.Items.distinct("0.Color")
+    else:
+        color = [color]
+    if factory == "None":
+        factory = db.Items.distinct("0.Factory")
+    else:
+        factory = [factory]
+    if powersupply == "None":
+        powersupply = db.Items.distinct("0.PowerSupply")
+    else:
+        powersupply = [powersupply]
+    if productionyear == "None":
+        productionyear = db.Items.distinct("0.ProductionYear")
+    else:
+        productionyear = [productionyear]
+    item = db.Items.find_one({'0.Category': {'$in': category}, '0.Model': {'$in':model}, '0.Factory': {'$in':factory}, '0.PowerSupply': {'$in':powersupply}, '0.ProductionYear':{'$in':productionyear}})
+    my_tree.insert(parent='', index='end',iid=count,text='',values=(item['0']['ItemID'],item['0']['Category'],item['0']['Model'],0,item['0']['Color'],item['0']['Factory'],item['0']['PowerSupply'],item['0']['ProductionYear']), tags=('evenrow',))
+    
+    #buttons
+    button_frame = LabelFrame(root, text="")
+    button_frame.pack(fill="x", expand="yes", padx=20)
+
+    buy_button = Button(button_frame, text="Purchase Product",command = customerPurchaseItem(itemId_entry, username))
+    buy_button.grid(row=0, column=0, padx=10, pady=10)
+    messagebox.showerror(title="SUCCESS",message="Item purchased!")
+
+    # Bind the treeview
+    my_tree.bind("<ButtonRelease-1>",select_record)
+        
+    def select_record(my_tree, e):
+        # Clear entry boxes
+        itemId_entry.delete(0, END)
+        model_entry.delete(0, END)
+
+        # Grab record Number
+        selected = my_tree.focus()
+        # Grab record values
+        values = my_tree.item(selected, 'values')
+
+        itemId = values[0]
+        
+        # outpus to entry boxes
+        itemId_entry.insert(0, values[0])
+        model_entry.insert(0, values[1])
+        
+
+
+    def clear_entries():
+        # Clear entry boxes
+        itemId_entry.delete(0, END)
+        warranty_entry .delete(0, END)
+        c_entry.delete(0, END)
+        ss_entry.delete(0, END)
+        m_entry.delete(0, END)
+        pd_entry.delete(0,END)
